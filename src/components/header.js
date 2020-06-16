a31df6567126c3461835764e6b73e5c89f809879
Tiny Cheeks <cole.reichenberg@gmail.com>
2020-06-15 18:27:54 -0600

    updates from video

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,37 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import headerStyles from "./header.module.scss"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header className={headerStyles.header}>
+    
+      <h1>
         <Link
+          className={headerStyles.title}
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
         >
           {siteTitle}
         </Link>
       </h1>
-    </div>
+      <nav>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link className={headerStyles.navItem} to="/">Home</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} to="/">Page 2</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} to="/">Page 3</Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} to="/">Page 4</Link>
+          </li>
+        </ul>
+        
+      </nav>
+    
   </header>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,13 +10,16 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
+import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
+          author
           title
         }
       }
@@ -24,23 +27,17 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header siteTitle={data.site.siteMetadata.title} />
+  
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        
+      
       </div>
-    </>
+      <Footer siteAuthor={data.site.siteMetadata.author}/>
+      
+    </div>
   )
 }
 
